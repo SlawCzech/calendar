be115312781ab804a964d3c3fee7439f8f56300e
SlawCzech <slaw.czech@gmail.com>
2022-10-24 23:05:35 +0200

    filtering calendar by date

--- calendar.py
+++ calendar.py
@@ -18,6 +18,25 @@ def calendar():
     return calendar_instance_
 
 
+def gt_start_date(start_date, event_start_date):
+    return True if start_date is None else start_date <= event_start_date
+
+
+def lt_end_date(end_date, event_end_date):
+    return True if end_date is None else end_date >= event_end_date
+
+
+def filter_by_date(calendar_data, start_date=None, end_date=None):
+    new_calendar_data = []
+
+    for event_instance in calendar_data:
+        if gt_start_date(start_date, event_instance['start_date']) \
+                and lt_end_date(end_date, event_instance['start_date'] + event_instance['duration']):
+            new_calendar_data.append(event_instance)
+
+    return new_calendar_data
+
+
 def get_event(calendar_data, event_name, start_date):
     event = None
     for event_instance in calendar_data:
@@ -55,9 +74,10 @@ def update_event(calendar_data, event_name, start_date, updates):
 
 calendar_instance = calendar()
 
-calendar_instance(add_event, event={'title': "dupa", 'start_date': 666, 'type': 'event'})
-calendar_instance(add_event, event={'title': "dupa", 'start_date': 43, 'type': 'event'})
-calendar_instance(add_event, event={'title': 'elo', 'start_date': 3, 'type': 'meeting'})
+calendar_instance(add_event, event={'title': "dupa", 'start_date': 666, 'type': 'event', 'duration': 6})
+calendar_instance(add_event, event={'title': "dupa", 'start_date': 43, 'type': 'event', 'duration': 8})
+calendar_instance(add_event, event={'title': 'elo', 'start_date': 3, 'type': 'meeting', 'duration': 9})
 calendar_instance(update_event, event_name='dupa', start_date=666, updates={'type': 'meeting'})
+print(calendar_instance(filter_by_date, start_date=11, end_date=55))
 # print(calendar_instance(delete_event, event_name='dupa', start_date=2))
 print(calendar_instance())
