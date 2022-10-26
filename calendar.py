86d8766f49c2fb5da6db4efef9621e4fe5b79c29
SlawCzech <slaw.czech@gmail.com>
2022-10-26 23:17:59 +0200

    added search and filtering

--- calendar.py
+++ calendar.py
@@ -1,7 +1,11 @@
+from functools import reduce
+from pprint import pprint as pp
+
+
 def sort_calendar(key):
     def wrapper(fn):
         def inner(*args, **kwargs):
-            return sorted(fn(*args, **kwargs), key=lambda element: element[key])
+            return sorted(fn(*args, **kwargs), key=lambda element: element[key])  # sort a sorted
 
         return inner
 
@@ -12,10 +16,17 @@ def calendar():
     calendar_data = []
 
     @sort_calendar(key='title')
-    def calendar_instance_(fn=None, **kwargs):
-        return fn(calendar_data, **kwargs) if fn is not None else calendar_data
+    def calendar_instance_(fns=None, **kwargs):
+        if isinstance(fns, (list, tuple)):
+            return reduce(lambda acc, fn: fn(acc, **kwargs), fns, calendar_data)
+        else:
+            return fns(calendar_data, **kwargs) if fns is not None else calendar_data
 
     return calendar_instance_
+
+
+def search_events(calendar_data, title='', **kwargs):
+    return [event_instance for event_instance in calendar_data if title.lower() in event_instance['title'].lower()]
 
 
 def gt_start_date(start_date, event_start_date):
@@ -26,7 +37,7 @@ def lt_end_date(end_date, event_end_date):
     return True if end_date is None else end_date >= event_end_date
 
 
-def filter_by_date(calendar_data, start_date=None, end_date=None):
+def filter_by_date(calendar_data, start_date=None, end_date=None, **kwargs):
     new_calendar_data = []
 
     for event_instance in calendar_data:
@@ -35,6 +46,16 @@ def filter_by_date(calendar_data, start_date=None, end_date=None):
             new_calendar_data.append(event_instance)
 
     return new_calendar_data
+
+
+def filter_by_duration(calendar_data, duration_min=0, duration_max=None, **kwargs):
+    calendar_data_by_duration_range = []
+
+    for event_instance in calendar_data:
+        if duration_min <= event_instance['duration'] <= (duration_max or event_instance['duration']):
+            calendar_data_by_duration_range.append(event_instance)
+
+    return calendar_data_by_duration_range
 
 
 def get_event(calendar_data, event_name, start_date):
@@ -78,6 +99,29 @@ calendar_instance(add_event, event={'title': "dupa", 'start_date': 666, 'type': 
 calendar_instance(add_event, event={'title': "dupa", 'start_date': 43, 'type': 'event', 'duration': 8})
 calendar_instance(add_event, event={'title': 'elo', 'start_date': 3, 'type': 'meeting', 'duration': 9})
 calendar_instance(update_event, event_name='dupa', start_date=666, updates={'type': 'meeting'})
-print(calendar_instance(filter_by_date, start_date=11, end_date=55))
+# print(calendar_instance(filter_by_date, start_date=11, end_date=55))
+
 # print(calendar_instance(delete_event, event_name='dupa', start_date=2))
-print(calendar_instance())
+# print(calendar_instance())
+# pp(filter_by_duration(calendar_instance(filter_by_date, start_date=30), duration_min=8))
+config = {
+    'start_date': 30,
+    'end_date': 700,
+    'duration_min': 4,
+    'duration_max': 7,
+}
+# pp(calendar_instance([filter_by_date, filter_by_duration], start_date=30, end_date=700, duration_min=4, duration_max=7))
+funcs = [filter_by_date, filter_by_duration]
+
+# pp(calendar_instance(funcs, **config)
+
+# pp(calendar_instance(search_events, title='Upa'))
+
+find_functions = [filter_by_duration, search_events]
+
+find_configs = {
+
+    'duration_max': 7
+}
+
+pp(calendar_instance(find_functions, **find_configs))
